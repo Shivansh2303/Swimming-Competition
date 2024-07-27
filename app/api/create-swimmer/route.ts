c06b4fbc764ed5d5b947b49e811f9ba49cfc0afa
Shiranka23 <shivansh.kate@digimonk.in>
2024-07-27 15:22:47 +0530

    added log statement

--- app/api/create-swimmer/route.ts
+++ app/api/create-swimmer/route.ts
@@ -3,11 +3,16 @@ import SwimmingCompetitionForm from "@/app/models/form.models";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
+  console.log({req:req});
+  
   try {
     
     const { userData } = await req.json();
     delete userData.proofOfAge;
+    console.log({userDataCreation:userData});
     const swimmer = await SwimmingCompetitionForm.create(userData);
+    console.log({swimmerResponse:swimmer});
+    
     return NextResponse.json(swimmer, { status: 200 });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
