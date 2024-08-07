485c5a82f65e38dbfb25d64f7577c0a748131d25
Shivansh2303 <shivanshkate@gmail.com>
2024-08-07 11:38:02 +0530

    swimmer save issue

--- lib/EmailService.ts
+++ lib/EmailService.ts
@@ -1,24 +1,22 @@
 import nodemailer from "nodemailer";
 
-export default async function EmailService(userData:any){
-    try{
-        console.log("userData",userData);
-        const transporter = nodemailer.createTransport({
-            host: process.env.NEXT_PUBLIC_SMTP_HOST,
-            port: parseInt(process.env.NEXT_PUBLIC_SMTP_PORT!),
-            secure: false, // Use `true` for port 465, `false` for all other ports
-            auth: {
-              user:  process.env.NEXT_PUBLIC_SMTP_USER,
-              pass: process.env.NEXT_PUBLIC_SMTP_PASS,
-            },
-          });
-      
-      
-          const mailOptions = {
-            from: 'info@swimforindiaacademy.com',
-            to: 'salisak929@hostlace.com',
-            subject: 'Swimming Competition Registration',
-            html: `
+export default async function EmailService(userData: any) {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.NEXT_PUBLIC_SMTP_HOST,
+      port: parseInt(process.env.NEXT_PUBLIC_SMTP_PORT!),
+      secure: false, // Use `true` for port 465, `false` for all other ports
+      auth: {
+        user: process.env.NEXT_PUBLIC_SMTP_USER,
+        pass: process.env.NEXT_PUBLIC_SMTP_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: "info@swimforindiaacademy.com",
+      to: "salisak929@hostlace.com",
+      subject: "Swimming Competition Registration",
+      html: `
              <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
       <p>Dear Swimmer,</p>
       <p>Thank you for registering in the Delhi Open Talent Search Swimming Competition 2024.</p>
@@ -33,13 +31,13 @@ export default async function EmailService(userData:any){
       <p>Dr. Jitender Tokas<br>Organizing Secretary</p>
     </div>
           `,
-          };
-          transporter.sendMail(mailOptions, function(error:any, info:any){
-            if (error) {
-              console.log('Error:', error);
-            } else {
-              console.log('Email sent: ' + info.response);
-            }
-          });
-    }catch(error){console.error("Error: ",error)}
+    };
+    transporter.sendMail(mailOptions, function (error: any, info: any) {
+      if (error) {
+        console.log("Error:", error);
+      }
+    });
+  } catch (error) {
+    console.error("Error: ", error);
+  }
 }
--- lib/SwimmerCreation.ts
+++ lib/SwimmerCreation.ts
@@ -1,8 +1,7 @@
 // app/api/create-customer/route.ts
 import SwimmingCompetitionForm from "@/app/models/form.models";
 import { connectToMongoDB } from "./db";
-import axios from "axios";
-import nodemailer from "nodemailer";
+import EmailService from "./EmailService";
 
 export async function SwimmerCreate(data: any) {
   try {
@@ -14,43 +13,7 @@ export async function SwimmerCreate(data: any) {
       return swimmerExist;
     }
     const swimmer = await SwimmingCompetitionForm.create(data);
-    const transporter = nodemailer.createTransport({
-      host: process.env.NEXT_PUBLIC_SMTP_HOST,
-      port: parseInt(process.env.NEXT_PUBLIC_SMTP_PORT!),
-      secure: false,
-      auth: {
-        user: process.env.NEXT_PUBLIC_SMTP_USER,
-        pass: process.env.NEXT_PUBLIC_SMTP_PASS,
-      },
-    });
-
-    const mailOptions = {
-      from: "info@swimforindiaacademy.com",
-      to: swimmer.email,
-      subject: "Swimming Competition Registration",
-      html: `
-       <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
-<p>Dear Swimmer,</p>
-<p>Thank you for registering in the Delhi Open Talent Search Swimming Competition 2024.</p>
-<p>Our Contact details are:</p>
-<p>Questions? Call/WhatsApp <strong>7065195811, 9310523009</strong></p>
-<p>Email: <a href="mailto:info@swimforindiaacademy.com" style="color: #1a73e8;">info@swimforindiaacademy.com</a></p>
-<p>Please keep checking your email as all communication is sent on email. We will send you meet program by Thursday, 22nd August.</p>
-<p>Wishing you all the best.</p>
-<p><strong>Keep Swimming, Keep Swimming!</strong></p>
-<p>With best wishes from Swim For India Academy,</p>
-<p>Respectfully Yours,</p>
-<p>Dr. Jitender Tokas<br>Organizing Secretary</p>
-</div>
-    `,
-    };
-    transporter.sendMail(mailOptions, function (error: any, info: any) {
-      if (error) {
-        console.log("Error:", error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
-    });
+    await EmailService(swimmer);
     return swimmer;
   } catch (error: any) {
     console.error("Error: ", error);
