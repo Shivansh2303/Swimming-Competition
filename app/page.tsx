8960101a6efcfa65c673be678c3b2eac68076aa8
Shivansh2303 <shivanshkate@gmail.com>
2024-08-23 02:56:18 +0530

    deactivate form

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import RegistrationClosed from "@/components/RegistrationForm/RegistrationClosed
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-5  bg-[url('/IMG_1639.JPG')] bg-repeat-round">
-      <SwimmingRegistrationForm/>
-    </main>
-    // <main className="flex min-h-screen flex-col items-center justify-between p-5  ">
-    //   <RegistrationClosed/>
+    // <main className="flex min-h-screen flex-col items-center justify-between p-5  bg-[url('/IMG_1639.JPG')] bg-repeat-round">
+    //   <SwimmingRegistrationForm/>
     // </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-5  ">
+      <RegistrationClosed/>
+    </main>
   );
 }
