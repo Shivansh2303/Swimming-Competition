4c64d72a337ed54bcc28f0129d28c15e1750373c
Shivansh2303 <shivanshkate@gmail.com>
2025-07-21 12:28:28 +0530

    testing email service

--- lib/EmailService.ts
+++ lib/EmailService.ts
@@ -2,6 +2,7 @@ import nodemailer from "nodemailer";
 
 export default async function EmailService(userData: any) {
   try {
+    console.log("11 userdata: ", userData )
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
       port: 465,
@@ -11,7 +12,7 @@ export default async function EmailService(userData: any) {
         pass: process.env.NEXT_PUBLIC_SMTP_PASS,
       },
     });
-
+    console.log("12 Email service initialized");
     const mailOptions = {
       from: `"Swim For India Academy" <kateshivansh2303@gmail.com>`,
       to: userData.email,
@@ -19,7 +20,7 @@ export default async function EmailService(userData: any) {
       replyTo: "info@swimforindiaacademy.com",
       html: `
          <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
-      <p>Dear Swimmer,</p>
+      <p>Dear Swimmer, ${userData.swimmerFirstName} ${userData.swimmerLastName}</p>
       <p>Thank you for registering in the Delhi Open Talent Search Swimming Competition 2025.</p>
       <p><strong>Event Date: 3rd August 2025, Sunday</strong></p>
       <p>Our Contact details are:</p>
@@ -41,6 +42,7 @@ export default async function EmailService(userData: any) {
         console.log("Email sent: ", info.response);
       }
     });
+    console.log("13 Email sent successfully");
   } catch (error) {
     console.error("Error: ", error);
   }
